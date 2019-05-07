f368138136ea9fb87168027762c18e13f2e51afb
Srebranets Artur <srebranetsartur@gmai.com>
2019-05-07 21:11:58 +0300

    -Add property holder bean

---
+++ src/main/java/org/srebranets/taskcloud/prop/OrderProps.java
@@ -0,0 +1,17 @@
+package org.srebranets.taskcloud.prop;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Component
+@ConfigurationProperties(prefix = "taco.order")
+@Data
+public class OrderProps {
+    @Min(value = 5, message = "must be between 5 and 25")
+    @Max(value = 25, message = "must be between 5 and 25")
+    private int pageSize;
+}
