9a02255bca9495b4417cacebdc3a7be78c2b74ee
Srebranets Artur <srebranetsartur@gmai.com>
2019-05-07 21:14:54 +0300

    -Add findByUserOrderByPlacedAtDesc method

--- src/main/java/org/srebranets/taskcloud/data/OrderRepository.java
+++ src/main/java/org/srebranets/taskcloud/data/OrderRepository.java
@@ -1,8 +1,12 @@
 package org.srebranets.taskcloud.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.srebranets.taskcloud.domain.Order;
+import org.srebranets.taskcloud.domain.User;
+
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
-
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
