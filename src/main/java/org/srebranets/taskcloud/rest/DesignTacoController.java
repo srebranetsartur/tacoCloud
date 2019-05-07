6c51a1f33e91bd4e34d796eeb887f83de718699e
Srebranets Artur <srebranetsartur@gmai.com>
2019-05-07 21:10:58 +0300

    -Add rest controller for /design request

---
+++ src/main/java/org/srebranets/taskcloud/rest/DesignTacoController.java
@@ -0,0 +1,44 @@
+package org.srebranets.taskcloud.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.srebranets.taskcloud.data.TacoRepository;
+import org.srebranets.taskcloud.domain.Order;
+import org.srebranets.taskcloud.domain.Taco;
+
+import java.util.Optional;
+
+@RestController("tacoRest")
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin("*")
+public class DesignTacoController {
+    private TacoRepository tacoRepository;
+
+    @Autowired
+    public DesignTacoController(TacoRepository tacoRepository) {
+        this.tacoRepository = tacoRepository;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepository.findById(id);
+        return optTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK)).orElseGet(() -> new ResponseEntity(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
+    }
+
+}
