c0f8da0729fb15882642908c01b7b04804ef9405
Srebranets Artur <srebranetsartur@gmai.com>
2019-05-07 21:12:37 +0300

    -Add annotation @EnableConfigurationProperties

--- src/main/java/org/srebranets/taskcloud/WebConfig.java
+++ src/main/java/org/srebranets/taskcloud/WebConfig.java
@@ -1,10 +1,12 @@
 package org.srebranets.taskcloud;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableConfigurationProperties
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
